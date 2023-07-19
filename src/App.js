0a5871803ea7b6402878f2fa5b886e9fabff346b
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-19 12:43:20 +0900

    20230719 #2 UseReducer 예제2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
 import React, {useReducer, useState} from 'react';
+import student from "./Student";
+import Student from "./Student";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'add-student' :
+            const name = action.payload.name;
+            const newStudent = {
+                id: Date.now(),
+                name: name,
+                isHere: false
+            }
+            return {
+                count: state.count + 1,
+                students: [...state.students, newStudent]
+            }
+        case'delete-student':
+            return {
+                count: state.count - 1,
+                students: state.students.filter(student => student.id !== action.payload.id)
+            }
+        case 'mark-student' :
+            return {
+                count: state.count,
+                students: state.students.map(student=>{
+                    if (student.id === action.payload.id){
+                        return {...student,isHere: !student.isHere}
+                    }
+                    return student;
+                })
+            }
+        default :
+            return state;
+    }
+};
+
+const initialState = {
+    count: 0,
+    students: []
+}
 
 function App() {
-    // reducer - state를 업데이트 하는 역할
-    // dispatch - state 업데이트를 위한 요구
-    // action - 요구의 내용
-    const ACTION_TYPES={
-        deposit : 'deposit',
-        withdraw : 'withdraw'
-    }
-
-    const reducer = (state, action) => {
-        switch (action.type){
-            case ACTION_TYPES.deposit :
-                return state + action.payload
-            case ACTION_TYPES.withdraw :
-                return state - action.payload
-            default :
-                return state
-        }
-    };
-
-   const [number, setNumber]  = useState(0);
-   const [money, dispatch] = useReducer(reducer,0);
+    const [name, setName] = useState('');
+    const [studentInfo, dispatch] = useReducer(reducer, initialState);
 
     return (
         <div>
-            <h2>useReducer 은행에 오신것을 환영합니다.</h2>
-            <p>잔고 : {money}원</p>
+            <h1>출석부</h1>
+            <p>총 학생 수 : {studentInfo.count}</p>
             <input
-                type={"number"}
-                value={number}
-                onChange={(e)=>setNumber(parseInt(e.target.value))}
-                step={"1000"}
+                type={"text"}
+                placeholder={"이름을 입력해주세요"}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
             />
-            <button onClick={()=>{
-                dispatch({type:ACTION_TYPES.deposit, payload:number});
-            }}>예금</button>
-            <button onClick={()=>{
-                dispatch({type:ACTION_TYPES.withdraw, payload:number});
-            }}>출금</button>
+            <button onClick={() => {
+                dispatch({type: 'add-student', payload: {name}})
+            }}>추가
+            </button>
+            {studentInfo.students.map((student) => {
+                return (
+                    <Student
+                        key={student.id}
+                        name={student.name}
+                        dispatch={dispatch}
+                        id={student.id}
+                        isHere={student.isHere}
+                    />
+                )
+            })}
         </div>
     );
 }
