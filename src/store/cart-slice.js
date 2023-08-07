1305f8aa6f57912025c6e8bf4ffc94b77f809cad
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-08 08:39:44 +0900

    20230808 #2 이벤트 핸들러 action 등록

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -3,16 +3,17 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
   items: [], totalQuantity: 0,
 }
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: initialState,
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id)
+      const existingItem = state.items.find((item) => item.id === newItem.id)
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -26,12 +27,16 @@ createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
-        // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     }
   }
 })
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
