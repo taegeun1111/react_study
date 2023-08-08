c3bf3ee8b045d80f3a247c516ba0d00e06fce5ec
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-08 10:24:21 +0900

    20230808 #5 Get Fetch

--- src/store/cart-action.js
+++ src/store/cart-action.js
@@ -1,15 +1,34 @@
 import {uiActions} from "./ui-slice";
+import {cartActions} from "./cart-slice";
 
-export const fetchCartData = () =>{
-  return dispatch => {
-    const fetchData = async () =>{
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
       const response = await fetch('https://reduxcart-993c7-default-rtdb.firebaseio.com/cart.json');
 
-      if (!response.ok){
+      if (!response.ok) {
         throw new Error('Could not fetch cart data')
       }
 
       const data = await response.json();
+
+      return data;
+    }
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart({
+        items : cartData.items || [],
+        totalQuantity : cartData.totalQuantity
+      }));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
+        })
+      );
     }
   }
 }
@@ -24,12 +43,12 @@ export const sendCartData = (cart) => {
       })
     );
 
-    const sendRequest = async () =>{
+    const sendRequest = async () => {
       const response = await fetch(
         'https://reduxcart-993c7-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({items: cart.items, totalQuantity: cart.totalQuantity}),
         }
       );
       if (!response.ok) {
@@ -47,7 +66,7 @@ export const sendCartData = (cart) => {
           message: 'Sent cart data successfully!',
         })
       );
-    }catch (error){
+    } catch (error) {
       dispatch(
         uiActions.showNotification({
           status: 'error',
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,16 +2,23 @@ import {createSlice} from "@reduxjs/toolkit";
 import {uiActions} from "./ui-slice";
 
 const initialState = {
-  items: [], totalQuantity: 0,
+  items: [],
+  totalQuantity: 0,
+  changed : false,
 }
 const cartSlice = createSlice({
   name: 'cart',
   initialState: initialState,
   reducers: {
+    replaceCart(state,action){
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id)
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -29,10 +36,12 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
+      state.changd = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     }
   }
