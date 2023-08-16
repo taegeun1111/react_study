b094aec00aef93b0602b9720e73a1026db8e7caf
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-16 18:30:36 +0900

    20230816 #2 props로 css 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+/** @jsxImportSource @emotion/react */
+
+const colors = {
+  default: "rgb(36,41,47)",
+  danger: "rgb(207,34,46)",
+  outline: "rgb(9,105,218)"
+}
+
+export function Button({children, variant = "default"}) {
+  return <button css={{
+    borderRadius: "6px",
+    backgroundColor: "rgb(246,248,250)",
+    color: colors[variant],
+    fontSize: "30px"
+  }}>{children}</button>
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Button variant={"default"}>Button</Button>
+      <Button variant={"danger"}>Button</Button>
+      <Button variant={"outline"}>Button</Button>
+    </>
   )
 }
 
