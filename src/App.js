0b4a6d44efd0499d4b5003e5d6ad464c77c78977
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-16 18:35:22 +0900

    20230816 #2 props로 css(font-size) 설정

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ const colors = {
   outline: "rgb(9,105,218)"
 }
 
-export function Button({children, variant = "default"}) {
+const sizeStyles = {
+  sm : {
+    fontSize : "12px",
+    padding : "3px 12px"
+  },
+  md : {
+    fontSize : "14px",
+    padding : "5px 16px"
+  },
+  lg : {
+    fontSize : "16px",
+    padding : "9px 20px"
+  }
+}
+
+export function Button({children, variant = "default", size = "md"}) {
   return <button css={{
     borderRadius: "6px",
     backgroundColor: "rgb(246,248,250)",
     color: colors[variant],
-    fontSize: "30px"
+    ...sizeStyles[size],
   }}>{children}</button>
 }
 
 function App() {
   return (
     <>
-      <Button variant={"default"}>Button</Button>
-      <Button variant={"danger"}>Button</Button>
-      <Button variant={"outline"}>Button</Button>
+      <Button variant={"default"} size={"sm"}>Button</Button>
+      <Button variant={"danger"} size={"md"}>Button</Button>
+      <Button variant={"outline"} size={"lg"}>Button</Button>
     </>
   )
 }
