3ad43d295dc7c9a9871932147bacf7194a957593
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-17 16:29:57 +0900

    20230817 #1 init

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-/** @jsxImportSource @emotion/react */
-
-const colors = {
-  default: "rgb(36,41,47)",
-  danger: "rgb(207,34,46)",
-  outline: "rgb(9,105,218)"
-}
-
-const sizeStyles = {
-  sm : {
-    fontSize : "12px",
-    padding : "3px 12px"
-  },
-  md : {
-    fontSize : "14px",
-    padding : "5px 16px"
-  },
-  lg : {
-    fontSize : "16px",
-    padding : "9px 20px"
-  }
-}
-
-export function Button({children, variant = "default", size = "md"}) {
-  return <button css={{
-    borderRadius: "6px",
-    backgroundColor: "rgb(246,248,250)",
-    color: colors[variant],
-    ...sizeStyles[size],
-  }}>{children}</button>
-}
 
 function App() {
   return (
     <>
-      <Button variant={"default"} size={"sm"}>Button</Button>
-      <Button variant={"danger"} size={"md"}>Button</Button>
-      <Button variant={"outline"} size={"lg"}>Button</Button>
+
     </>
   )
 }
