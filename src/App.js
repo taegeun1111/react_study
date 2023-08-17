e3eb9b3c4b298b7776598b892277a34a4c15d76d
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-17 16:42:34 +0900

    20230817 #2 props 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import styled from "styled-components"
+
+const SimpleButton = styled.button`
+  background: blue;
+  color: white;
+  border: none;
+`;
+
+const LargeButton = styled(SimpleButton)`
+  font-size: 50px;
+`
+
+const SmallButton = styled(LargeButton)`
+color: bisque;
+`
+const PrimaryButton = styled.button`
+  color: ${(props) => {
+    if (props.primary){
+      return 'red'
+    }
+    return 'blue'
+  }};
+`
 
 function App() {
   return (
     <>
-
+      <SimpleButton>Simple</SimpleButton>
+      <LargeButton>Large</LargeButton>
+      <SmallButton>Small</SmallButton>
+      <PrimaryButton>Normal</PrimaryButton>
+      <PrimaryButton primary>Primary</PrimaryButton>
     </>
   )
 }
