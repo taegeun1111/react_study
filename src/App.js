e808d04c4b6e107faf44d3fa4780add100fa445f
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-19 20:14:14 +0900

    20230819 #2 Mixin Group

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import styled, {createGlobalStyle, css} from "styled-components"
-import {useState} from "react";
 
-export const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: antiquewhite
-  }
-`;
+const Sticky = css`
+  position: fixed;
+  background-color: wheat;
+  transition: all 0.6s ease-in-out;
+  padding: 1rem;
+`
+
+const Navigation = styled.nav`
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 100;
+  ${Sticky}
+`
 
 function App() {
-  const [email, setEmail] = useState("");
-
   return (
-    <GlobalStyle>
-      Components...
-    </GlobalStyle>
+    <>
+      <Navigation>
+        Sticky
+      </Navigation>
+    </>
   )
 }
-
-
 
 
 export default App;
