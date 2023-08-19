a7e705d9c94bd373b7a182d0c536880b0bbfdd08
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-19 19:39:02 +0900

    20230819 #1 GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import styled, {css} from "styled-components"
+import styled, {createGlobalStyle, css} from "styled-components"
 import {useState} from "react";
 
-const Wrap = styled.div`
-  background: ${({active}) => {
-    if (active) {
-      return "white";
-    }
-    return "#eee";
-  }};
-  color: black;
-`
-const Button = styled.button`
-  width: 200px;
-  padding: 30px;
-`
-// 상속
-const NewButton = styled(Button)`
-  color: ${props => props.color || "red"};
-`
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: antiquewhite
+  }
+`;
 
 function App() {
   const [email, setEmail] = useState("");
 
   return (
-    <Wrap active={email.length}>
-      <Button>Hello</Button>
-      <NewButton color={"blue"}>New Button</NewButton>
-    </Wrap>
+    <GlobalStyle>
+      Components...
+    </GlobalStyle>
   )
 }
 
-const flexCenter = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const FlexBox = styled.div`
-  ${flexCenter}
-`;
-
-console.log(FlexBox)
 
 
 
