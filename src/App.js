22bb662a737bcc012071ef9a9cc7dcc451e70b12
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-19 20:18:29 +0900

    20230819 #3 Mixin Group parameter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import styled, {createGlobalStyle, css} from "styled-components"
 
-const Sticky = css`
-  position: fixed;
-  background-color: wheat;
-  transition: all 0.6s ease-in-out;
-  padding: 1rem;
+const RingVariant = (radius, stroke = 10) => css`
+  position: absolute;
+  border-radius: 50%;
+  height: ${radius *2}px;
+  width: ${radius * 2}px;
+  border: ${stroke}px solid rgba(0,0,0,0.5);
 `
 
-const Navigation = styled.nav`
+const ShapeRing = styled.div`
+  ${RingVariant(40,15)};
+  top: 30%;
   left: 0;
-  top: 0;
-  right: 0;
-  z-index: 100;
-  ${Sticky}
+  background-color: aquamarine;
 `
+
+
 
 function App() {
   return (
     <>
-      <Navigation>
+      <ShapeRing>
         Sticky
-      </Navigation>
+      </ShapeRing>
     </>
   )
 }
